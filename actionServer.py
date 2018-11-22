504bccc363b7bf134cb41c86ff4a147bd946a70f
Jakub Trllo <jakub.trllo@kreden.cz>
2018-11-22 17:58:58 +0100

    Servers are classes now, also basic logger added

--- actionServer.py
+++ actionServer.py
@@ -20,38 +20,39 @@ FTRACK_ACTIONS_PATH # Paths to folders where are located actions e.g. "M:/Ftrack
 PYTHONPATH # Path to ftrack_api and paths to all modules used in actions e.g. path to ftrack_action_handler
 """
 
+class FtrackActionServer():
+    def __init__(self):
+        # Separate all paths
+        self.actionsPaths = os.environ["FTRACK_ACTIONS_PATH"].split(os.pathsep)
 
-def run_server():
-    # Separate all paths
-    actionsPaths = os.environ["FTRACK_ACTIONS_PATH"].split(os.pathsep)
+    def stop_action_session(self):
+        self.actionSession.event_hub.disconnect()
+        self.actionSession = None
 
-    # Create session to Ftrack
-    session = ftrack_api.Session(auto_connect_event_hub=True,)
+    def set_actions(self):
+        # Iterate all paths
+        for path in self.actionsPaths:
+            # add path to PYTHON PATH
+            sys.path.append(path)
+            # Get all modules with functions
+            for m in os.listdir(path):
+                # Get only .py files with action functions
+                if '.pyc' in m or '.py' not in m:
+                    continue
+                try:
+                    mod = importlib.import_module(os.path.splitext(m)[0])
+                    mod_functions = dict([(name, function)
+                    for name, function in mod.__dict__.items() if isinstance(
+                        function, types.FunctionType)])
 
-    # Iterate all paths
-    for path in actionsPaths:
-        # add path to PYTHON PATH
-        sys.path.append(path)
-        # Get all modules with functions
-        for m in os.listdir(path):
-            # Get only .py files with action functions
-            if '.pyc' in m or '.py' not in m:
-                continue
-            try:
-                mod = importlib.import_module(os.path.splitext(m)[0])
-                mod_functions = dict([(name, function)
-                                      for name, function in mod.__dict__.items() if isinstance(
-                    function, types.FunctionType)])
+                        # Run register on each action
+                    mod_functions['register'](self.actionSession)
 
-            # Run register on each action
-                mod_functions['register'](session)
+                except KeyError as e:
+                    log.warning("'{0}' - not proper action ({1})".format(m,e))
 
-            except Exception as e:
-                log.error("File load error: '{0}' is not proper action - {1}".format(m, e))
-
-    # keep event_hub on session running
-    session.event_hub.wait()
-
-
-if __name__ == '__main__':
-    run_server()
+    def run_action_server(self):
+        self.actionSession = ftrack_api.Session(auto_connect_event_hub=True,)
+        self.set_actions()
+        # keep event_hub on session running
+        self.actionSession.event_hub.wait()
--- eventServer.py
+++ eventServer.py
@@ -21,38 +21,38 @@ FTRACK_EVENTS_PATH # Paths to folders where are located actions e.g. "M:/FtrackA
 PYTHONPATH # Path to ftrack_api and paths to all modules used in actions e.g. path to ftrack_action_handler
 """
 
+class FtrackEventServer():
+    def __init__(self):
+        # Separate all paths
+        self.eventsPaths = os.environ["FTRACK_EVENTS_PATH"].split(os.pathsep)
 
-def run_server():
-    # Separate all paths
-    eventsPaths = os.environ["FTRACK_EVENTS_PATH"].split(os.pathsep)
+    def set_events(self):
+        # Create session to Ftrack ( auto_connect_event_hub - multitask
+        self.eventSession = ftrack_api.Session(auto_connect_event_hub=True,)
 
-    # Create session to Ftrack ( auto_connect_event_hub - multitask
-    session = ftrack_api.Session(auto_connect_event_hub=True,)
-
-    # Iterate all paths
-    for path in eventsPaths:
-        # add path to PYTHON PATH
-        sys.path.append(path)
-        # Get all modules with functions
-        for m in os.listdir(path):
-            # Get only .py files
-            if '.pyc' in m or '.py' not in m:
-                continue
-            try:
+        # Iterate all paths
+        for path in self.eventsPaths:
+            # add path to PYTHON PATH
+            sys.path.append(path)
+            # Get all modules with functions
+            for m in os.listdir(path):
+                # Get only .py files
+                if '.pyc' in m or '.py' not in m:
+                    continue
                 mod = importlib.import_module(os.path.splitext(m)[0])
                 allEventFunctions = dict([(name, function)
                                           for name, function in mod.__dict__.items() if isinstance(function, types.FunctionType)])
 
                 # Register all events
                 for f in allEventFunctions:
-                    session.event_hub.subscribe('topic=ftrack.update', allEventFunctions[f])
+                    self.eventSession.event_hub.subscribe('topic=ftrack.update', allEventFunctions[f])
 
-            except Exception as e:
-                log.error("File load error: '{0}' is not proper action - {1}".format(m, e))
+    def stop_event_session(self):
+        self.eventSession.event_hub.disconnect()
+        self.eventSession = None
 
-    # Keep event_hub on session running
-    session.event_hub.wait()
-
-
-if __name__ == '__main__':
-    run_server()
+    def run_event_server(self):
+        self.eventSession = ftrack_api.Session(auto_connect_event_hub=True,)
+        self.set_events()
+        # keep event_hub on session running
+        self.eventSession.event_hub.wait()
