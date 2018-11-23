05dd5f6452933f97e5fe46a24642371a99e48657
Jakub Trllo <jakub.trllo@kreden.cz>
2018-11-23 16:32:01 +0100

    single class for both servers and 2 easy run files

--- FtrackServer.py
+++ FtrackServer.py
@@ -35,49 +35,26 @@ class FtrackServer():
         """
         self.type = type
 
-        self.actionAvailable = True
-        self.eventAvailable = True
+        self.actionsAvailable = True
+        self.eventsAvailable = True
         # Separate all paths
         try:
             self.actionsPaths = os.environ["FTRACK_ACTIONS_PATH"].split(os.pathsep)
         except:
             self.actionsAvailable = False
-            log.warning("FTRACK_ACTIONS_PATH is not set, action server won't launch")
         try:
             self.eventsPaths = os.environ["FTRACK_EVENTS_PATH"].split(os.pathsep)
         except:
             self.eventsAvailable = False
-            log.warning("FTRACK_EVENTS_PATH is not set, event server won't launch")
 
     def stop_session(self):
         self.session.event_hub.disconnect()
         self.session.close()
         self.session = None
 
-    def set_events(self):
-        # Create session to Ftrack ( auto_connect_event_hub - multitask
-        self.eventSession = ftrack_api.Session(auto_connect_event_hub=True,)
-
+    def set_files(self, paths):
         # Iterate all paths
-        for path in self.eventsPaths:
-            # add path to PYTHON PATH
-            sys.path.append(path)
-            # Get all modules with functions
-            for m in os.listdir(path):
-                # Get only .py files
-                if '.pyc' in m or '.py' not in m:
-                    continue
-                mod = importlib.import_module(os.path.splitext(m)[0])
-                allEventFunctions = dict([(name, function)
-                                          for name, function in mod.__dict__.items() if isinstance(function, types.FunctionType)])
-
-                # Register all events
-                for f in allEventFunctions:
-                    self.eventSession.event_hub.subscribe('topic=ftrack.update', allEventFunctions[f])
-
-    def set_actions(self):
-        # Iterate all paths
-        for path in self.actionsPaths:
+        for path in paths:
             # add path to PYTHON PATH
             sys.path.append(path)
             # Get all modules with functions
@@ -92,16 +69,20 @@ class FtrackServer():
                         function, types.FunctionType)])
 
                         # Run register on each action
-                    mod_functions['register'](self.actionSession)
+                    mod_functions['register'](self.session)
 
                 except KeyError as e:
-                    log.warning("'{0}' - not proper action ({1})".format(m,e))
+                    log.warning("'{0}' - not proper {1} ({2})".format(m, self.type, e))
 
     def run_server(self):
         self.session = ftrack_api.Session(auto_connect_event_hub=True,)
         if self.type.lower() == 'event':
-            self.set_events()
+            if self.eventsAvailable is False:
+                log.error("FTRACK_EVENTS_PATH is not set, event server won't launch")
+            self.set_files(self.eventsPaths)
         else:
-            self.set_actions()
+            if self.actionsAvailable is False:
+                log.error("FTRACK_ACTIONS_PATH is not set, action server won't launch")
+            self.set_files(self.actionsPaths)
         # keep event_hub on session running
         self.session.event_hub.wait()
