0f8ec8b9567c84a2ca59b6d16d29b606b9ee3586
Jakub Trllo <jakub.trllo@gmail.com>
2019-02-11 13:15:35 +0100

    added possibility to login through console

--- eventServer.py
+++ eventServer.py
@@ -1,4 +1,109 @@
+import os
+import json
+import ftrack_api
 from FtrackServer import FtrackServer
+from app import api
 
-server = FtrackServer('event')
-server.run_server()
+log = api.Logger.getLogger(__name__)
+
+def check_cred(user, key):
+    os.environ["FTRACK_API_USER"] = user
+    os.environ["FTRACK_API_KEY"] = key
+
+    try:
+        session = ftrack_api.Session()
+        session.close()
+        return True
+    except Exception:
+        return False
+
+def ask_yes_no():
+    possible_yes = ["y", "yes"]
+    possible_no = ["n", "no"]
+    log.info("Y/N:")
+    cont = input()
+    if cont.lower() in possible_yes:
+        return True
+    elif cont.lower() in possible_no:
+        return False
+    else:
+        log.info(
+            "Invalid input. Possible entries: [y, yes, n, no]. Try it again:"
+        )
+        return ask_yes_no()
+
+def cli_login():
+    templates = os.environ['PYPE_STUDIO_TEMPLATES']
+    path_items = [templates, 'presets', 'ftrack', 'ftrack_event_server.json']
+    event_credentials_file = os.path.sep.join(path_items)
+
+    enter_cred = True
+
+    with open(event_credentials_file, 'r') as fp:
+        cred_data = json.load(fp)
+
+    user = cred_data.get("FTRACK_API_USER", None)
+    key = cred_data.get("FTRACK_API_KEY", None)
+    auto = cred_data.get("AUTO_CONNECT", False)
+    if user is None or key is None:
+        log.info("Credentials are not set. Do you want to enter them now? (Y/N)")
+        if ask_yes_no() is False:
+            log.info("Exiting...")
+            return
+    elif check_cred(user, key):
+        if auto is False:
+            log.info("Do you want to log with username {}? (Y/N)".format(
+                cred_data["FTRACK_API_USER"]
+            ))
+            if ask_yes_no():
+                enter_cred = False
+        else:
+            enter_cred = False
+    else:
+        log.info(
+            "Stored credentials are not valid. "
+            "Do you want enter them now?(Y/N)"
+        )
+        if ask_yes_no() is False:
+            log.info("Exiting...")
+            return
+
+    while enter_cred:
+        log.info("Please enter Ftrack API User:")
+        user = input()
+        log.info("And now enter Ftrack API Key:")
+        key = input()
+        if check_cred(user, key):
+            export = {
+                "FTRACK_API_USER": user,
+                "FTRACK_API_KEY": key
+            }
+            log.info(
+                "Credentials are valid."
+                " Do you want to auto-connect next time?(Y/N)"
+            )
+            if ask_yes_no():
+                export["AUTO_CONNECT"] = True
+
+            with open(event_credentials_file, 'w') as fp:
+                json.dump(export, fp)
+            enter_cred = False
+            break
+        else:
+            log.info(
+                "Entered credentials are not valid."
+                " Do you want to try it again?(Y/N)"
+            )
+            if ask_yes_no() is False:
+                log.info("Exiting...")
+                return
+
+    server = FtrackServer('event')
+    server.run_server()
+
+
+def main():
+    cli_login()
+
+if (__name__ == ('__main__')):
+    main()
